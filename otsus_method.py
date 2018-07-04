b176bd8087678bf4b3833ff14929ee62cc90fd0f
milanmt <milanmariya@gmail.com>
2018-07-04 18:04:59 +0530

    As we're using RANSAC, redo RANSAC twice to prevent any unpredictable change, if there is a change of more than 10%

--- otsus_method.py
+++ otsus_method.py
@@ -10,30 +10,30 @@ import find_threshold
 
 if __name__ == '__main__':
 	
-	company_path = '/media/milan/DATA/Qrera/Cannula'
+	company_path = '/media/milan/DATA/Qrera/AutoAcc'
 	files = []
 
 	for root, dirs, fs in os.walk(company_path):
 		if fs:
-			files.extend(os.path.join(root,f) for f in fs)
+			files.extend(os.path.join(root,f) for f in fs if f.endswith('.csv.gz'))
 
 	threshold_days = []
 	days = []
 	
-	
 	for file in files:
 		print (file[-17:-7])
 		t0 = time.clock()
-		threshold = find_threshold.threshold_of_device(company_path, 1, file[-17:-7])
-		print (threshold)
+		threshold, RANSAC = find_threshold.threshold_of_device(company_path, 1, file[-17:-7])
 
+		## Repeat once to supress errors due to randomness
+		if np.absolute(threshold - np.mean(threshold_days[-5:]))/np.mean(threshold_days[-5:]) > 0.1:
+			threshold = find_threshold.threshold_of_device(company_path, 1, file[-17:-7])
+		
 		threshold_days.append(threshold)
 		days.append(file[:-7])
-		print ('Time in secs: ', time.clock()- t0)
 
-
-	df = pandas.DataFrame( data = list(zip(threshold_days, days)), columns = ['Threshold', 'Day'])
-	df.to_csv('cannula_filtered_otsus.csv', index= True, header=True)
+	df = pandas.DataFrame( data = list(zip(threshold_days, days, ransac)), columns = ['Threshold', 'Day', 'RANSAC'])
+	df.to_csv('autoacc_filtered_otsus.csv', index= True, header=True)
 
 
 
