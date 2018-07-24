2ee3da5caaf5b97383a06f42f3ebb852423f8d2f
milanmt <milanmariya@gmail.com>
2018-07-24 15:08:52 +0530

    Added code to cluster peaks to discrete states, via variational bayesian gaussian clustering. Organised code for use with other modules.

--- peak_detector.py
+++ peak_detector.py
@@ -1,18 +1,25 @@
 #! /usr/bin/env python3
 
+from sklearn.mixture import BayesianGaussianMixture
+from datetime import datetime, timedelta
 from scipy.signal import find_peaks, butter, filtfilt
 import matplotlib.pyplot as plt
+import pattern_discovery
 import find_threshold 
 import pandas as pd 
 import numpy as np 
 import time
+import os
 
 
-SMOOTHING_FILTER_WINDOW = 10
-THRESHOLD = 2610
-MIN_IDLE_TIME = 10  # seconds
-MIN_WORK_TIME = 10  # seconds 
+# THRESHOLD = 2610
 
+def zero_cross_detector(sig):
+	indices = []
+	for i in range(len(sig)-1):
+		if sig[i] > 0 and sig[i+1] < 0:
+			indices.append(i)
+	return np.array(indices)
 
 def var_round(number):
 	number = float(number)
@@ -35,9 +42,20 @@ def lpf(data):
 	return y
 
 
-def preprocess_power(df):
-	print ('Preprocessing...')
-	df.sort_values(by='TS')	
+def preprocess_power(f1, f2):
+	print ('Preprocessing files to extract data...')
+
+	df1 = pd.read_csv(f1)
+	df2 = pd.read_csv(f2)
+	df1.sort_values(by='TS')
+	df2.sort_values(by='TS')
+	df = pd.concat([df1,df2])	
+
+	start_time = datetime.isoformat(datetime.strptime(f1[-17:-7]+' 08:00:00', '%Y_%m_%d %H:%M:%S'), sep=' ')
+	end_time = datetime.isoformat(timedelta(days=1) + datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S'), sep=' ')
+
+	df = df[(df['TS'] >= start_time) & (df['TS'] < end_time)]
+
 	df['TS'] = df['TS'].apply(lambda x: int(time.mktime(time.strptime(x, '%Y-%m-%d %H:%M:%S'))))
 
 	power = []
@@ -56,54 +74,142 @@ def preprocess_power(df):
 					power.append(df.iloc[i-1,0])
 
 
-	## Thresholding Signal 
-	power = pd.Series(power).apply(lambda x: x if x > THRESHOLD else THRESHOLD)
+	## Thresholding Signal   ## This is necessary if you plan to distinguish cycles based on threshold.
+	# power = pd.Series(power).apply(lambda x: x if x > THRESHOLD else THRESHOLD)
+
+	## Smoothing 
+	power_f = lpf(power)
 
 	### Differencing filter
 	p_detrend = []
-	for i in range(len(power)-1):
-		p_detrend.append(power[i+1]-power[i])
+	for i in range(len(power_f)-1):
+		p_detrend.append(power_f[i+1]-power_f[i])
 
-	## lpf
-	power_f = lpf(p_detrend)
+	## Smoothing derivative
+	power_d = lpf(p_detrend)
 
-	# plt.plot(power)
-	# plt.plot(p_detrend)
-	# plt.plot(power_f)
-	# plt.show()
 
-	return power_f
+	return power_d, power_f
+
+
+def get_required_files(device_path, day):
+
+	print ('Obtaining Required Files...')
+
+	file1 = None
+	file2 = None
+	end_search = False
+
+	for root, dirs, files in os.walk(device_path):
+		if files and not end_search:
+			files.sort()
+			for f in files:
+				if day in f and f.endswith('.csv.gz'):
+					file1 = os.path.join(root,f)
+				
+				if file1 and f > file1:
+					file2 =  os.path.join(root,f)
+					end_search = True
+					break
+
+	return file1, file2
+
+
+def detect_peaks(power_d, power_f):
+	print ('Detecting Peaks of Signal....')
+	# power_d - smoothed derivative of signal;
+	# power_f - filtered signal
+	# peaks, _ = find_peaks(power_d)  # Find peaks returns the actual peaks of derivative
+	
+	peaks = zero_cross_detector(power_d)  # Returns indices 
+	peaks = peaks+1
+	
+	## For obtaining actuals signal with peaks.
+	# peak_p = np.zeros((len(power_d)))
+	# peak_p[peaks] = power_f[peaks]
+
+	final_peaks = power_f[peaks]
+
+	return final_peaks, peaks
+
+def peaks_to_discrete_states(final_peaks)
+	#### BayesianGaussianMixture
+
+	total_peaks = np.array(final_peaks)
+	X = total_peaks.reshape(-1,1)
+
+	dpgmm = BayesianGaussianMixture(n_components=10,covariance_type='full').fit(X)
+	labels = dpgmm.predict(X)
+
+
+	return labels
 
 
 if __name__ == '__main__':
+
+	device_path = '/media/milan/DATA/Qrera/FWT/5CCF7FD0C7C0'
+	day = '2018_07_06'
+
+	file1, file2 = get_required_files(device_path, day)
+
+	power_d, power_f = preprocess_power(file1, file2)
+
+	final_peaks, peak_indices = detect_peaks(power_d, power_f)
+
+	labels = peaks_to_discrete_states(final_peaks)
+
+	################ Visualization
+
+	# total_peaks_len = len(final_peaks)
+	# print (total_peaks_len)
+	# plt.plot(range(total_peaks_len),final_peaks)
+	# plt.show()
+
+	# color=['navy', 'c', 'cornflowerblue', 'gold','darkorange', 'r', 'g', 'm', 'y', 'k']
 	
-	# file = '/media/milan/DATA/Qrera/AutoAcc/39FFBE/2018/05/2018_05_08.csv.gz'
-	file = '/media/milan/DATA/Qrera/FWT/5CCF7FD0C7C0/2018/07/2018_07_05.csv.gz'
+	# color_labels = []
+	# for label in labels:
+	# 	color_labels.append(color[int(label)])
 
-	df = pd.read_csv(file)
 
-	power_f = preprocess_power(df)
+	# plt.scatter(range(total_peaks_len), final_peaks, color= color_labels)
+	# plt.show()
 
-	peaks, _ = find_peaks(power_f)
-	peak_p = power_f[peaks]
+	# print (dpgmm.means_)
+	# print (dpgmm.covariances_)
+	# print (dpgmm.weights_)
+
+
+	#################################################################################################
+
+	##################### Finding patterns where correlation was maximum.
+
+	# n_power_f = (power_f - np.mean(power_f))/(np.std(power_f)*len(power_f))
+
+	# pattern = np.array(power_f[180:200])
+	# n_pattern = (pattern - np.mean(pattern))/(np.std(pattern))
 	
-	peak_pr = [var_round(p) for p in peak_p]
-	peak_threshold = find_threshold.get_otsus_threshold(peak_pr)
-	print ('peak_threshold', peak_threshold)
+	# correlation = np.correlate(n_power_f, n_pattern,'full')
 
-	# peak_threshold = 3000
-	final_peaks = np.zeros((len(power_f)))
-	for p in peaks:
-		if power_f[p] > peak_threshold:
-			final_peaks[p] = power_f[p]
+	# peaks, _ = find_peaks(correlation, height=0.0005)
+	# print ('Total Signals Detected: ', len(peaks))
+
+	# plt.plot(correlation[0:1500])
+	# plt.show()
+
+	######################
 	
-	plt.plot(power_f)
-	plt.stem(final_peaks)
-	plt.show()
-	total_peaks = len([p for p in final_peaks if p != 0])
 
-	print ('Total Peaks:', total_peaks)
+	####################### Filtering peaks with a threshold
+	# peak_pr = [var_round(p) for p in peak_p]
+	# peak_threshold = find_threshold.get_otsus_threshold(peak_pr)
+	# print ('peak_threshold', peak_threshold)
 
-
-
-
+	# final_peaks = np.zeros((len(power_f)))
+	# for p in peaks:
+	# 	if power_f[p] > peak_threshold:
+	# 		final_peaks[p] = power_f[p]
+	
+	# total_peaks = [p for p in final_peaks if p != 0]
+	
+	########################
