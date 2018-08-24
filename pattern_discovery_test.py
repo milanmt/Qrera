6f1e40fdfb32a5ca5345557be90e4ca97c163685
milanmt <milanmariya@gmail.com>
2018-08-24 15:16:23 +0530

    Updated with most recent discover p[atterns, cluster pattern methods which utilised levenshtein distance

--- pattern_discovery_test.py
+++ pattern_discovery_test.py
@@ -162,38 +162,49 @@ class SequentialPatternMining:
 
 	
 	def discover_pattern(self):
-		seq_support = self.__get_all_freq_seq()
-		seq_support_m = self.__get_maximal_patterns(seq_support)
-
+		working_patterns, idle_patterns, pattern_dict = self.cluster_patterns()
+		
+		### Looking for patterns that start and stop in the same state
 		possible_patterns = []
-		for seq,support in seq_support_m:
+		for seq in working_patterns:
 			if seq[0] == seq[-1]:
 				possible_patterns.append(seq)
+		print ('possible_patterns')
+		print (possible_patterns)
 
 		### Finding max variance among patterns that start and end the same state
 		if possible_patterns:
 			max_var_ind = self.__get_max_var_ind(possible_patterns) 
-			final_pattern = possible_patterns[max_var_ind] 
+			final_pattern = possible_patterns[max_var_ind]
+			print(final_pattern) 
+			return final_pattern
 		else:
 			## If no such pattern exists, extend patterns that gives likely output
+			add_pattern = None
 			print ('Case when pattern not found directly')
-			max_var_ind = self.__get_max_var_ind([seq[0] for seq in seq_support_m])
-			max_var_pattern = seq_support_m[max_var_ind][0] 
+			max_var_ind = self.__get_max_var_ind([seq for seq in working_patterns])
+			max_var_pattern = working_patterns[max_var_ind]
+			print(max_var_pattern)
 			min_len = np.inf
-			for seq in seq_support_m:
-				if seq[0][0] == max_var_pattern[-1] and seq[0][-1] == max_var_pattern[0]:
-					if len(seq[0]) < min_len:
-							min_len = len(seq[0])
-							add_pattern = seq[0]
-			max_var_pattern.extend(add_pattern[1:])
-			final_pattern = max_var_pattern
-		print (final_pattern)
+			for seq in working_patterns:
+				if seq[0] == max_var_pattern[-1] and seq[-1] == max_var_pattern[0]:
+					if len(seq) < min_len:
+							min_len = len(seq)
+							add_pattern = seq
 
-		def cluster_patterns(self):
-		seq_support = self.__get_all_freq_seq()
-		seq_f =  self.__get_maximal_patterns(seq_support)	
+			if add_pattern != None:
+				max_var_pattern.extend(add_pattern[1:])
+				final_pattern = max_var_pattern
+				print (final_pattern)
+				return final_pattern
+			else:
+				return None
+		
 
+	def cluster_patterns(self):
+		seq_f =  self.maximal_patterns	
 		### Clustering maximal sequences using affinity propagation
+		### Computing similarity/affinity matrix using levenshtein distance
 		p_dist = np.zeros((len(seq_f), len(seq_f)))
 		for i in range(len(seq_f)):
 			for j in range(i,len(seq_f)):
@@ -204,7 +215,7 @@ class SequentialPatternMining:
 					p_dist[j][i] = p_dist[i][j]
 		p_dist = p_dist/np.max(p_dist)
 		p_dist = 1 - p_dist
-
+		### Affinity Propagation
 		ap = AffinityPropagation(affinity='precomputed')
 		ap.fit(p_dist)
 		cluster_subseqs_exs = [ seq_f[ind][0] for ind in ap.cluster_centers_indices_]
@@ -212,7 +223,7 @@ class SequentialPatternMining:
 	
 		print(cluster_subseqs_exs)
 
-		### Arranging sequences by cluster label -> mostly for future use
+		### Arranging sequences by cluster label 
 		cluster_subseqs = dict()
 		for seq, label in zip(seq_f,subseq_labels):
 			if label not in cluster_subseqs:
@@ -224,7 +235,40 @@ class SequentialPatternMining:
 
 		print (cluster_subseqs)
 
-		return cluster_subseqs
+		### Getting average variances of the entire clusters for detecting different regions
+		cluster_variances = list(np.zeros(len(cluster_subseqs)))
+		for label, seq_l in cluster_subseqs.items():
+			var_seq_l = []
+			for seq_supp in seq_l:
+				seq = seq_supp[0]
+				var = np.std([self.state_attributes[str(s)][0] for s in seq])
+				var_seq_l.append(var)
+			cluster_variances[label] = np.mean(var_seq_l)
+
+		print (cluster_variances)
+		### Clustering variances
+		ap_v = AffinityPropagation(affinity='euclidean')
+		cl_v_labels = ap_v.fit_predict(np.array(cluster_variances).reshape(-1,1))
+		print (cl_v_labels)
+		cl_v_exs = [ cluster_variances[ind] for ind in ap_v.cluster_centers_indices_]
+		print (cl_v_exs)
+		working_label = cl_v_labels[cluster_variances.index(max(cl_v_exs))]
+		
+		### Seperating working and idle based on variance of patterns
+		working_patterns = []
+		idle_patterns = []
+		for e,l in enumerate(cl_v_labels):
+			if l == working_label:
+				working_patterns.extend([seq[0] for seq in cluster_subseqs[e]])
+			else:
+				idle_patterns.extend([seq[0] for seq in cluster_subseqs[e]])
+
+		print ("Working Patterns")
+		print (working_patterns)
+		print ('Idle Patterns')
+		print (idle_patterns)
+		return working_patterns, idle_patterns
+
 
 def seq_contains(seq, subseq):
 	seq_s = str()
