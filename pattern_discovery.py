fa4a027695f49cbe7ba1f96953c31192d4394086
milanmt <milanmariya@gmail.com>
2018-08-27 13:58:55 +0530

    Removed finding maximal pattern method. Now looking for highest support among possible patterns. If there is no max or no possible patterns,form by extension.

--- pattern_discovery.py
+++ pattern_discovery.py
@@ -14,17 +14,66 @@ class SequentialPatternMining:
 	def __init__(self, sequence, state_attributes):
 		### Mining generative patterns only
 		self.MAX_LEN = 10
-		self.MIN_SUPPORT = 0.3
+		self.MIN_LEN = 3
+		self.MIN_SUPPORT = 0.33
 		self.N_SEGMENTS = 48
 		self.sequence = list(sequence) if not isinstance(sequence, list) else sequence
 		self.state_attributes = state_attributes
 		self.states = [s for s in self.state_attributes.keys()]
+		self.diff_matrix = self.get_diff_matrix()
 		self.db_filename = 'timedb_test.txt'
 		self.pattern_filename = 'output.txt'
 		self.path_to_spmf = '/media/milan/DATA/Qrera'
-		self.generator_patterns, self.maximal_patterns = self.__get_maximal_generator_patterns()
+		self.similarity_constraint = 1  ## No single element can appear more than 100x% of the time
+		self.generator_patterns = self.__get_all_freq_seq() 
+			
+	
+	def get_diff_matrix(self):
+		S = len(self.states)
+		diff_matrix = np.zeros((S,S))
+		for s in range(S):
+			for sn in range(s,S):
+				diff_matrix[s][sn] = abs(self.state_attributes[self.states[s]][0] - 
+					self.state_attributes[self.states[sn]][0])
+				if sn != s:
+					diff_matrix[sn][s] = diff_matrix[s][sn]
+
+		diff_matrix = (S*diff_matrix)//np.max(diff_matrix)+1
+		for s in range(S):
+			diff_matrix[s][s] = 0
+		# print (diff_matrix)	
+		return diff_matrix
 
 	
+	def levenshtein_distance(self,a,b):
+		lev_m = np.zeros((len(a)+1,len(b)+1))
+		a.insert(0,0)
+		b.insert(0,0)
+		max_score = np.max(self.diff_matrix)+1
+		for i in range(len(a)):
+			for j in range(len(b)):
+				### forming matrix
+				if i == 0 and j == 0:
+					lev_m[i][j] = 0
+				elif i == 0:
+					lev_m[i][j] = lev_m[i][j-1]+max_score
+				elif j == 0:
+					lev_m[i][j] = lev_m[i-1][j]+max_score
+				else:
+					### getting scores
+					score = self.diff_matrix[self.states.index(str(a[i]))][self.states.index(str(b[j]))]
+					if any(a_l in b for a_l in a):
+						score_ins_a = score//2
+					else:
+						score_ins_a = max_score
+					if any(b_l in a for b_l in b):
+						score_ins_b = score//2
+					else:
+						score_ins_b = max_score
+					lev_m[i][j] = min(lev_m[i-1][j]+score_ins_a, lev_m[i][j-1]+score_ins_b, lev_m[i-1][j-1]+score)
+		return lev_m[len(a)-1][len(b)-1]
+
+
 	def __generate_timeseries_db(self):
 		len_segment = len(self.sequence)//self.N_SEGMENTS
 		with open(self.db_filename, 'w') as f:
@@ -60,7 +109,8 @@ class SequentialPatternMining:
 							support = int(s.split(':')[1].strip())
 						else:
 							seq.append(int(s))
-					seq_support.append((seq, support))
+					if len(seq) >= self.MIN_LEN:
+						seq_support.append((seq, support))
 		return seq_support
 
 
@@ -73,62 +123,71 @@ class SequentialPatternMining:
 		return max_var_ind
 
 
-	def __get_maximal_generator_patterns(self):
-		seq_support = self.__get_all_freq_seq()
-		maximal_patterns = []
-		for seq in seq_support:
-			for seql in seq_support:
-				if seq_contains(seql[0], seq[0]) and seql[0] != seq[0]:
-					if seql not in maximal_patterns:
-						maximal_patterns.append(seql)
-					if seq in maximal_patterns:
-						ind = maximal_patterns.index(seq)
-						del maximal_patterns[ind]
-		return seq_support, maximal_patterns
+	def __get_most_common_subseq(self, possible_patterns):
+		subseq_count = list(np.zeros(len(possible_patterns)))
+		for e,seq in enumerate(possible_patterns):
+			for seql in possible_patterns:
+				if seq_contains(seql, seq):
+					subseq_count[e] += 1
+
+		max_ind = subseq_count.index(max(subseq_count))
+
+		if all(c == max(subseq_count) for c in subseq_count):
+			return None
+		else:
+			return possible_patterns[max_in]
 
 	
+	def __get_pattern_by_extension(self, working_patterns):
+		add_pattern = None
+		print ('Case when pattern not found directly')
+		max_var_ind = self.__get_max_var_ind([seq for seq in working_patterns])
+		max_var_pattern = working_patterns[max_var_ind]
+		print(max_var_pattern)
+		min_len = np.inf
+		for seq in working_patterns:
+			if seq[0] == max_var_pattern[-1] and seq[-1] == max_var_pattern[0]:
+				if len(seq) < min_len:
+						min_len = len(seq)
+						add_pattern = seq
+
+		if add_pattern != None:
+			max_var_pattern.extend(add_pattern[1:])
+			final_pattern = max_var_pattern
+			print (final_pattern)
+			return final_pattern
+		else:
+			return None
+
 	def discover_pattern(self):
 		working_patterns, idle_patterns, pattern_dict = self.cluster_patterns()
 		
 		### Looking for patterns that start and stop in the same state
 		possible_patterns = []
 		for seq in working_patterns:
-			if seq[0] == seq[-1]:
+			_, count = np.unique(seq, return_counts=True)
+			count = count/sum(count)
+			if seq[0] == seq[-1] and all(c < self.similarity_constraint for c in count):
 				possible_patterns.append(seq)
 		print ('possible_patterns')
 		print (possible_patterns)
 
 		### Finding max variance among patterns that start and end the same state
 		if possible_patterns:
-			max_var_ind = self.__get_max_var_ind(possible_patterns) 
-			final_pattern = possible_patterns[max_var_ind]
-			print(final_pattern) 
-			return final_pattern
-		else:
-			## If no such pattern exists, extend patterns that gives likely output
-			add_pattern = None
-			print ('Case when pattern not found directly')
-			max_var_ind = self.__get_max_var_ind([seq for seq in working_patterns])
-			max_var_pattern = working_patterns[max_var_ind]
-			print(max_var_pattern)
-			min_len = np.inf
-			for seq in working_patterns:
-				if seq[0] == max_var_pattern[-1] and seq[-1] == max_var_pattern[0]:
-					if len(seq) < min_len:
-							min_len = len(seq)
-							add_pattern = seq
-
-			if add_pattern != None:
-				max_var_pattern.extend(add_pattern[1:])
-				final_pattern = max_var_pattern
-				print (final_pattern)
+			final_pattern = self.__get_most_common_subseq(possible_patterns) 
+			if final_pattern != None:
+				print(final_pattern) 
 				return final_pattern
 			else:
-				return None
+				return self.__get_pattern_by_extension(working_patterns)
+
+		else:
+			## If no such pattern exists, extend patterns that gives likely output
+			return self.__get_pattern_by_extension(working_patterns)
 		
 
 	def cluster_patterns(self):
-		seq_f =  self.maximal_patterns	
+		seq_f =  self.generator_patterns
 		
 		### Getting means and variances of the patterns
 		pattern_mv = list(np.zeros((len(seq_f),2)))
