0df2a2603226b28ab53db11e4cabed5ae1ed9c11
milanmt <milanmariya@gmail.com>
2018-09-24 17:32:38 +0530

    We need min_length as a parameter to improve efficiency and accuracy.

--- custompattern_mining.py
+++ custompattern_mining.py
@@ -20,12 +20,14 @@ def timing_wrapper(func):
 	return wrapper
 
 class PatternMining:
-	def __init__(self, sequence, state_attributes, max_len):
+	def __init__(self, sequence, state_attributes, max_len, min_len):
 		self.sequence = sequence
 		self.MAX_LEN = max_len   ## Max len should be greater than longest possible pattern
+		self.MIN_LEN = min_len
 		self.state_attributes = state_attributes
 		self.min_states, self.max_states = self.__partition_states()
-		self.pattern_sets = []
+		self.__pattern_sets = dict()
+		self.patterns_unique = []
 
 	def __partition_states(self):
 		seq_means = [s[0] for s in self.state_attributes.values()]
@@ -44,23 +46,15 @@ class PatternMining:
 
 	
 	def __get_end_ind(self, p_temp):
-		start = 0
-		for i in range(1,len(p_temp)):
-			if p_temp[0] != p_temp[i]:
-				start = i
-				break
-
-		if start == 0:
-			return len(p_temp)
-		else:
-			try:
-				end = start + p_temp[start:].index(p_temp[0]) +1
-			except ValueError:
-				if start >= 3:
-					end = start
-				else:
-					end = len(p_temp)
-			return end
+		start = self.MIN_LEN
+		try:
+			end = start + p_temp[start:].index(p_temp[0]) +1
+		except ValueError:
+			if start >= 3:
+				end = start
+			else:
+				end = len(p_temp)
+		return end
 
 	@timing_wrapper
 	def find_patterns(self):
@@ -71,28 +65,36 @@ class PatternMining:
 			if self.sequence[init_ind] in self.min_states:
 				p_temp = self.sequence[init_ind:init_ind+self.MAX_LEN]
 				end_ind = self.__get_end_ind(p_temp)
-				p = self.sequence[init_ind : init_ind+end_ind]
+				p = tuple(self.sequence[init_ind : init_ind+end_ind])
 
-				if len(p) == 3 :
-					print (p)
 				if p[0] == p[-1]:
 					p_set = self.__patternset_list_contains(p)
 					if p_set == None:
-						p_set = [p]
-						self.pattern_sets.append(p_set)
+						self.__pattern_sets.update({p : {p :1}})
 					else:
-						p_set.append(p)
+						if p not in p_set:
+							p_set.update({p:1})
+						else:
+							p_set[p] +=1
 
-		print ('Total Unique Patterns: ', len(self.pattern_sets))
+		### Finding most frequent pattern
+		for p_set in self.__pattern_sets.values():
+			total_freq = sum(p_set.values())
+			max_item = max([z for z in p_set.items()], key= lambda x:x[1])
+			self.patterns_unique.append((list(max_item[0]),total_freq))
+
+		print ('Total Unique Patterns: ', len(self.patterns_unique))
+		print (self.patterns_unique)
 		
-		if len(self.pattern_sets) == 0:
+		if len(self.__pattern_sets) == 0:
 			raise ValueError('No Patterns Found')
-		return self.pattern_sets
+		
+		return self.patterns_unique
 
 	def __patternset_list_contains(self, pattern):
-		for p_set in self.pattern_sets:
-			if self.__pattern_distance(pattern, p_set[0]) == 0.0:
-				return p_set
+		for p_head in self.__pattern_sets:
+			if self.__pattern_distance(pattern, p_head) == 0.0:
+				return self.__pattern_sets[p_head]
 		return None
 
 	def __pattern_distance(self, head, pattern):
