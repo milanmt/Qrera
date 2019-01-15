853570592cbe41790121f6b151549e83833d5f3d
milanmt <milanmariya@gmail.com>
2019-01-15 13:33:28 +0530

    Changes in pre_process power for correct timestamps.Changes in notification system to ensure dependency on previous minutes file only.

--- real_time_signal_detection.py
+++ real_time_signal_detection.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import RANSACRegressor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.mixture import BayesianGaussianMixture
 from sklearn.naive_bayes import GaussianNB
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 import plotly.graph_objs as go
 import numpy as np
 import pandas
@@ -24,33 +24,37 @@ def timing_wrapper(func):
 @timing_wrapper
 def preprocess_power(df):
 	print ('Preprocessing power...')
-	### Preprocessing
+	### Preprocessing for plotting 
+	
 	df['TS'] = df['TS'].apply(lambda x: int(datetime.timestamp(datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))))
 	# print (df.shape[0], 'orig_signal length')
 	power = np.zeros((86400))
-	power[0] = df.iloc[0,0]
 	offset = int(df.iloc[0,1])
 	t = offset
+	start_time = datetime.fromtimestamp(offset)
+	actual_start = datetime.combine(start_time.date(), time(0,0))
+	start_ind = int(datetime.timestamp(start_time) - datetime.timestamp(actual_start))
+	power[start_ind] = df.iloc[0,0]
 	for i in range(1,df.shape[0]):
 		if int(df.iloc[i,1]) != t:
 			if round(df.iloc[i,1]-t) == 1.0:
-				power[t+1-offset] = df.iloc[i,0]
+				power[start_ind+t+1-offset] = df.iloc[i,0]
 				t+=1			
 			elif int(df.iloc[i,1])-t < 21.0:
 				orig_t = t
 				req_offset = orig_t+1-offset
 				avg = (df.iloc[i,0]+df.iloc[i-1,0])/2
 				for j in range(int(df.iloc[i,1]-orig_t)):
-					power[req_offset+j] = avg
+					power[start_ind+req_offset+j] = avg
 					t+=1
 			else:
 				orig_t = t
 				req_offset = orig_t+1-offset
 				for j in range(int(df.iloc[i,1]-orig_t)):
-					power[req_offset+j] = 0
+					power[start_ind+req_offset+j] = 0
 					t+=1
 		else: 
-			power[t-offset] = (power[t-offset]+df.iloc[i,0])/2
+			power[start_ind+t-offset] = (power[start_ind+t-offset]+df.iloc[i,0])/2
 	return power
 
 @timing_wrapper
@@ -175,6 +179,17 @@ def get_current_status(log_path):
 	if os.path.isfile(log_path):
 		with open(log_path, 'r') as f:
 			status_dict = json.load(f)
+
+		last_updated = datetime.strptime(status_dict['last_updated'], '%Y-%m-%d %H:%M:%S')
+		if last_updated > datetime.now() - timedelta(minutes=2):
+			status_dict['current_status'] = 0
+			status_dict['Working'] = 0
+			status_dict['Loading'] = 0
+			status_dict['Idle'] = 0
+			status_dict['Off'] = 0
+			status_dict['notification_sent'] = False
+			status_dict['last_updated'] = datetime.isoformat(datetime.now())
+
 	else:
 		status_dict = dict()
 		status_dict['current_status'] = 0
@@ -183,8 +198,8 @@ def get_current_status(log_path):
 		status_dict['Idle'] = 0
 		status_dict['Off'] = 0
 		status_dict['notification_sent'] = False
-		with open(log_path, 'w') as f:
-			json.dump(status_dict, f, indent=4)
+		status_dict['last_updated'] = datetime.isoformat(datetime.now())
+
 	return status_dict
 
 def update_status_log(log_path, detected_region):
@@ -208,79 +223,23 @@ if __name__ == '__main__':
 
 	
 	device_path = '/media/milan/DATA/Qrera/PYN/B4E62D388561'
-	day = '2018_11_12'
+	day = '2018_10_10'
 	file1 = get_required_file(device_path, day)
 	power_df = initial_processing(file1)
 
 
 	## THRESHOLD
 	##################################################################################################################################################################################################################
-	Th1 = 2000   # Distinguish between idle and working
-	Th2 = 1000   # Distinguish between loading and idle
-	expected_loading_time = 4 # Minutes 
-	expected_idle_time = 4    # Minutes
-	notifier_log_path = 'data/notifier_log_path.json'
+	# Th1 = 2000   # Distinguish between idle and working
+	# Th2 = 1000   # Distinguish between loading and idle
+	# expected_loading_time = 4 # Minutes 
+	# expected_idle_time = 4    # Minutes
+	# notifier_log_path = 'data/notifier_log_path.json'
 
-	### Classification over 60 sec intervals
-	print ('Classifying signal over 60 sec intevrals...')
-	classified_signal_id = []
-	region_labels = ['Working', 'Loading', 'Idle', 'Off']
-	start_int = datetime.strptime(day+' 00:00:00', '%Y_%m_%d %H:%M:%S')
-	for i in range(1440):
-		# print (i,end='\r')
-		end_int = start_int + timedelta(minutes=1)   ## Window Required
-		start_str = datetime.isoformat(start_int, sep=' ')
-		end_str = datetime.isoformat(end_int, sep=' ')
-		req_df = power_df[(power_df['TS']>=start_str) & (power_df['TS']<end_str)]
-		if req_df.empty:
-			label = 3
-		else:
-			try:
-				avg_power = req_df['POWER'].mean()
-			except KeyError:
-				avg_power = req_df['VALUE'].mean()
-			
-			if avg_power == 0.0:
-				label = 3
-			elif avg_power >= Th1:
-				label = 0
-			else:
-				if avg_power >= Th2:
-					label = 1
-				else:
-					label = 2
-
-		print(i, avg_power, label) 
-		# status_dict = update_status_log(notifier_log_path, region_labels[label])
-		# # print (status_dict)
-		# if status_dict['Loading'] > expected_loading_time and not status_dict['notification_sent']:
-		# 	print ('NOTIFICATION FOR EXCEEDING EXPECTED LOADING TIME')
-		# 	status_dict['notification_sent'] = True
-		# if status_dict['Idle'] > expected_idle_time and not status_dict['notification_sent']:
-		# 	print ('NOTIFICATION FOR EXCEEDING EXPECTED IDLE TIME')
-		# 	status_dict['notification_sent'] = True
-
-		# with open(notifier_log_path, 'w') as f:
-		# 	json.dump(status_dict, f, indent=4)
-
-		classified_signal_id.append(label)
-		start_int = end_int
-
-	### ML 
-	##################################################################################################################################################################################################################
-	# training_file = '/media/milan/DATA/Qrera/trials/data/mean_dict.json'
-
-	# ### Discretise over trained power signals (One weeks data)
-	# # dpgmm, state_means = get_state_means(device_path, day)
-
-	# ### Get required classification from training data
-	# # region_classifier, region_labels = get_mv_classifier(training_file)
-	# region_classifier, region_labels = get_classifier(training_file)
-	
 	# ### Classification over 60 sec intervals
 	# print ('Classifying signal over 60 sec intevrals...')
-	# classified_signal = []
 	# classified_signal_id = []
+	# region_labels = ['Working', 'Loading', 'Idle', 'Off']
 	# start_int = datetime.strptime(day+' 00:00:00', '%Y_%m_%d %H:%M:%S')
 	# for i in range(1440):
 	# 	print (i,end='\r')
@@ -289,51 +248,115 @@ if __name__ == '__main__':
 	# 	end_str = datetime.isoformat(end_int, sep=' ')
 	# 	req_df = power_df[(power_df['TS']>=start_str) & (power_df['TS']<end_str)]
 	# 	if req_df.empty:
-	# 		region = 'Off'
-	# 		label = len(region_labels)
+	# 		label = 3
 	# 	else:
 	# 		try:
-	# 			# state_power = dpgmm.predict(np.array(req_df['POWER']).reshape(-1,1))
-	# 			sum_power = req_df['POWER'].sum()
-	# 			power_window = req_df['POWER']
+	# 			avg_power = req_df['POWER'].mean()
 	# 		except KeyError:
-	# 			# state_power = dpgmm.predict(np.array(req_df['VALUE']).reshape(-1,1))
-	# 			sum_power = req_df['VALUE'].sum()
-	# 			power_window = req_df['VALUE']
-
-	# 		# avg_power = np.mean([state_means[s] for s in state_power])
-	# 		# # print (avg_power, 'avg')
-	# 		# var_power = np.std([state_means[s] for s in state_power])
-	# 		# # print (var_power, 'std')
-	# 		avg_power = sum_power/req_df.shape[0]
+	# 			avg_power = req_df['VALUE'].mean()
 			
-	# 		if sum_power == 0.0:
-	# 			region = 'Off'
-	# 			label = len(region_labels)
+	# 		if avg_power == 0.0:
+	# 			label = 3
+	# 		elif avg_power >= Th1:
+	# 			label = 0
 	# 		else:
-	# 			# region_id = region_classifier.predict(np.array([[avg_power,var_power]]))
-				
-	# 			# region_id = region_classifier.predict(np.array([[avg_power]]))
-				
-	# 			region_ids = region_classifier.predict(np.array(power_window).reshape(-1,1))
-	# 			regions_present, counts_regions = np.unique(region_ids, return_counts=True)
-	# 			region_id = regions_present[np.argmax(counts_regions)]
-				
-	# 			region = region_labels[int(region_id)]
-	# 			label = int(region_id)
-	# 			# print (region, 'classified')
+	# 			if avg_power >= Th2:
+	# 				label = 1
+	# 			else:
+	# 				label = 2
 
-	# 	classified_signal.append(region)
+	# 	# status_dict = update_status_log(notifier_log_path, region_labels[label])
+	# 	# # print (status_dict)
+	# 	# if status_dict['Loading'] > expected_loading_time and not status_dict['notification_sent']:
+	# 	# 	print ('NOTIFICATION FOR EXCEEDING EXPECTED LOADING TIME')
+	# 	# 	status_dict['notification_sent'] = True
+	# 	# if status_dict['Idle'] > expected_idle_time and not status_dict['notification_sent']:
+	# 	# 	print ('NOTIFICATION FOR EXCEEDING EXPECTED IDLE TIME')
+	# 	# 	status_dict['notification_sent'] = True
+	# 	# status_dict['last_updated'] = datetime.isoformat(datetime.now())
+
+	# 	# with open(notifier_log_path, 'w') as f:
+	# 	# 	json.dump(status_dict, f, indent=4)
+
 	# 	classified_signal_id.append(label)
 	# 	start_int = end_int
+
+	### ML 
+	##################################################################################################################################################################################################################
+	training_file = '/media/milan/DATA/Qrera/trials/data/mean_dict.json'
+
+	### Discretise over trained power signals (One weeks data)
+	# dpgmm, state_means = get_state_means(device_path, day)
+
+	### Get required classification from training data
+	# region_classifier, region_labels = get_mv_classifier(training_file)
+	region_classifier, region_labels = get_classifier(training_file)
+	
+	### Classification over 60 sec intervals
+	print ('Classifying signal over 60 sec intevrals...')
+	classified_signal = []
+	classified_signal_id = []
+	start_int = datetime.strptime(day+' 00:00:00', '%Y_%m_%d %H:%M:%S')
+	for i in range(1440):
+		print (i,end='\r')
+		end_int = start_int + timedelta(minutes=1)   ## Window Required
+		start_str = datetime.isoformat(start_int, sep=' ')
+		end_str = datetime.isoformat(end_int, sep=' ')
+		req_df = power_df[(power_df['TS']>=start_str) & (power_df['TS']<end_str)]
+		if req_df.empty:
+			region = 'Off'
+			label = len(region_labels)
+		else:
+			try:
+				# state_power = dpgmm.predict(np.array(req_df['POWER']).reshape(-1,1))
+				sum_power = req_df['POWER'].sum()
+				power_window = req_df['POWER']
+			except KeyError:
+				# state_power = dpgmm.predict(np.array(req_df['VALUE']).reshape(-1,1))
+				sum_power = req_df['VALUE'].sum()
+				power_window = req_df['VALUE']
+
+			# avg_power = np.mean([state_means[s] for s in state_power])
+			# # print (avg_power, 'avg')
+			# var_power = np.std([state_means[s] for s in state_power])
+			# # print (var_power, 'std')
+			avg_power = sum_power/req_df.shape[0]
+			
+			if sum_power == 0.0:
+				region = 'Off'
+				label = len(region_labels)
+			else:
+				# region_id = region_classifier.predict(np.array([[avg_power,var_power]]))
+				
+				# region_id = region_classifier.predict(np.array([[avg_power]]))
+				
+				region_ids = region_classifier.predict(np.array(power_window).reshape(-1,1))
+				regions_present, counts_regions = np.unique(region_ids, return_counts=True)
+				region_id = regions_present[np.argmax(counts_regions)]
+				
+				region = region_labels[int(region_id)]
+				label = int(region_id)
+				# print (region, 'classified')
+
+		classified_signal.append(region)
+		classified_signal_id.append(label)
+		start_int = end_int
+
+	region_labels.append('Off')
+
+	############################################################################################################################################### COMMON PLOTTING
 
 	print ('Plotting...')
 	unique_labels = list(np.unique(classified_signal_id))
 	print (unique_labels, region_labels)
 	power = preprocess_power(power_df)
-	y_plot = np.zeros((len(unique_labels),len(power)))
+	y_plot = np.zeros((len(unique_labels),86400))
 	for e,el in enumerate(classified_signal_id):
-		y_plot[unique_labels.index(el),e*60:(e+1)*60] = power[e*60:(e+1)*60]
+		if region_labels[el] == 'Off':
+			y_plot[unique_labels.index(el),e*60:(e+1)*60] = 1500
+		else:
+			y_plot[unique_labels.index(el),e*60:(e+1)*60] = power[e*60:(e+1)*60]
+
 	time = np.arange(86400)
 	
 	plotly.tools.set_credentials_file(username='MilanMariyaTomy', api_key= '8HntwF4rtsUwPvjW3Sl4')
